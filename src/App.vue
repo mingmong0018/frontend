95f096378717d1572cac0dcaee8ae1bedd905f08
1kimm <anyionee@naver.com>
2021-01-19 19:14:36 +0900

    update main size by 1kimm

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,12 @@
         <b-navbar type="light" variant="light">
           <router-link to="/"><b-navbar-brand ><img src="@/static/images/shareroom_logo2.png" style="width:60px;"></b-navbar-brand></router-link>
           <b-navbar-nav class="ml-auto">
-            <b-nav-item href="#">방 찾기</b-nav-item>
             <b-nav-item href="#">찜 목록</b-nav-item>
-            <b-nav-item href="#">커뮤니티</b-nav-item>
             <b-nav-item href="#">알림<b-badge pill>+99</b-badge></b-nav-item>
 
             <!-- Navbar dropdowns -->
             <template v-if="id===null">
-              <b-nav-item href="#">로그인</b-nav-item>
+              <b-nav-item v-b-modal.loginModal>로그인</b-nav-item>
             </template>
             <template v-else>
               <b-nav-item-dropdown text="누구누구님" right>
@@ -28,18 +26,26 @@
       </div>
     </div>
     <router-view/>
+    <loginModal/>
   </div>
 </template>
 <script lang="ts">
-
-export default {
-  data(){
-    return{
-      id:null
+  import loginModal from '@/components/ModalLogin.vue'
+  export default {
+    components: {
+    loginModal
+    },
+    data(){
+      return{
+        id:null
+      }
     }
   }
-}
 </script>
 <style scoped>
-
+  #nav{
+    top:0px;
+    width:100vw;
+    position:fixed;
+  }
 </style>
