bd869814aaefed4cd35608ee51d2de33f7341800
mingmong0018 <mingmong0018@google.com>
2021-03-09 00:35:45 +0900

    reset keyword

---
+++ src/views/ListRoom.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container">
+        
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    @import './assets/media_set.css';
+    /* @media (min-width: 768px) {
+        .container {
+            width: 750px;
+        }
+    }
+
+    @media (min-width: 992px) {
+        .container {
+            width: 940px;
+        }
+    } */
+
+.container {
+    border: 1px solid red;
+}
+</style>
